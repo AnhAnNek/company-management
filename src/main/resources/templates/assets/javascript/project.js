70a71ad09b04997cb24d330b038d5eaec9c7b4ff
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-07-21 18:32:18 +0700

    fix code html

--- src/main/resources/templates/assets/javascript/project.js
+++ src/main/resources/templates/assets/javascript/project.js
@@ -13,3 +13,32 @@ var closeBtn = document.getElementsByClassName("close")[0];
 closeBtn.onclick = function() {
   closeModalProj();
 };
+
+//call api
+//get all:
+// // Function to make the API call and display the result
+// async function fetchDataFromAPI() {
+//   const apiUrl = 'http://localhost:8000/api/project'; // Replace this with the actual API endpoint URL
+//
+//   try {
+//       const response = await fetch(apiUrl);
+//       if (!response.ok) {
+//           throw new Error('Network response was not ok.');
+//       }
+//       const data = await response.json();
+//       displayData(data);
+//   } catch (error) {
+//       console.error('Error fetching data:', error.message);
+//       displayData({ error: 'Failed to fetch data from the API.' });
+//   }
+// }
+
+// // Function to display the API response on the web page
+// function displayData(data) {
+//   const resultElement = document.getElementById('result');
+//   resultElement.innerHTML = JSON.stringify(data, null, 2);
+// }
+//
+// // Add a click event listener to the button
+// const getDataButton = document.getElementById('getDataButton');
+// getDataButton.addEventListener('click', fetchDataFromAPI);
